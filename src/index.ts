926064b3b1c36de1c9960e271e89d817ca8fb2eb
Shane Lamb <shanelamb1@gmail.com>
2024-02-04 19:48:15 +1000

    export more types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export { BigqueryModelBuilder } from './builder'
+export { BigqueryModelBuilder, BigqueryModelBuilderConfig } from './builder'
+export { BigQueryModel, ModelType, TableFullName, TablePartialName, NameResolver, FullRefreshBigQueryModel } from './types'
+export { getLocalBigQuery, localNameTransform, localBigQueryProject } from './local-bigquery'
